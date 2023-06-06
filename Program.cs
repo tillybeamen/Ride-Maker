f174be8237a5fdf6e81b702e3f594da826eacd7b
Tillman Pugh <Till.pugh@gmail.com>
2023-06-06 13:55:20 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Vehicle Honda = new Vehicle("Honda", 4, "Black", false);
+Vehicle Bentley = new Vehicle("Bentley", 2, "White", true);
+Vehicle Jaguar = new Vehicle("Jaguar", 2, "Red", true);
+Vehicle Buick = new Vehicle("Buick", 5, "Green", true);
+
+List<Vehicle> AllVehicles = new List<Vehicle>();
+AllVehicles.Add(Honda);
+AllVehicles.Add(Bentley);
+AllVehicles.Add(Jaguar);
+AllVehicles.Add(Buick);
+
+foreach(Vehicle v in AllVehicles)
+{
+    v.ShowInfo();
+}
+
+System.Console.WriteLine(Honda.Name);
+System.Console.WriteLine(Bentley.Passengers);
+System.Console.WriteLine(Jaguar.Color);
+System.Console.WriteLine(Buick.Engine);
